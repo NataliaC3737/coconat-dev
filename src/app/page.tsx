091b038cd4ca7836782fd8363d622e85b6596961
Natalia <caleronatalia3737@gmail.com>
2024-10-11 11:32:57 +0200

    feat: upload afacad font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex justify-center items-center h-screen w-full bg-[#F2F2F2]">
+    <main className="flex flex-col gap-5 justify-center items-center h-screen w-full bg-[#F2F2F2]">
       <p className="text-4xl text-[#593825] bagel-fat-one-regular">Próximamente...</p>
+      <p className="text-2xl text-[#593825] afacad-regular">Portolio de Coconat Dev en curso</p>
     </main>
   );
 }
